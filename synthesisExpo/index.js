c56c789ac7629c1c878ddab80ea103b7c7570599
Kieran Poropat <kieran.poropat@gmail.com>
2025-07-05 15:12:16 -0400

    app comment

--- synthesisExpo/index.js
+++ synthesisExpo/index.js
@@ -4,5 +4,6 @@ import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// the environment is set up appropriately\
+// should be good to go
 registerRootComponent(App);
